ef825fba6f8138f7739d05ea47b6b8d903417e2f
Lucas Mudile Santana <lucas.santana@ucall.co.ao>
2024-02-11 07:52:30 +0000

    Upload de imagem.

---
+++ Api-GC/Controllers/ImageController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api_GC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageController : ControllerBase
+    {
+
+
+        private readonly IConfiguration _confuguration;
+        private readonly IWebHostEnvironment _env;
+
+        public ImageController(IConfiguration confuguration, IWebHostEnvironment env)
+        {
+            _confuguration = confuguration;
+            _env = env;
+        }
+
+
+
+
+
+        [Route("SaveFile")]
+        [HttpPost]
+
+        public JsonResult SaveFile()
+        {
+            try
+            {
+                var httpRequest = Request.Form;
+                var postedFile = httpRequest.Files[0];
+                string filename = postedFile.FileName;
+                var physicalPath = _env.ContentRootPath + "/Photos/" + filename;
+
+
+                using (var stream = new FileStream(physicalPath, FileMode.Create))
+                {
+                    postedFile.CopyTo(stream);
+
+                }
+
+                var url = $"{this.Request.Scheme}://{this.Request.Host}{this.Request.PathBase}";
+
+                return new JsonResult(url+ "/Photos/" + filename);
+
+            }
+            catch (Exception)
+            {
+                return new JsonResult("sasuke-1.png");
+            }
+        }
+
+
+    }
+}
---
+++ Api-GC/Program.cs
@@ -0,0 +1,50 @@
+using Microsoft.Extensions.FileProviders;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Enables Core
+builder.Services.AddCors(c =>
+{
+    c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod()
+     .AllowAnyHeader());
+
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(Path.Combine(app.Environment.ContentRootPath, "Photos")),
+    RequestPath = "/Photos"
+});
+
+app.UseCors(x => x
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .SetIsOriginAllowed(origin => true) // allow any origin
+                .AllowCredentials()); // allow credentials
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+
